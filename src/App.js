0fb5ec4cffc937758a7b2105ac3202fac8ba087a
Sijia1998 <zhu19980304@163.com>
2019-04-06 16:28:07 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import store from './store';
-import View from './components/TodoView';
-import styles from './App.less';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
-    return <div className={styles['content-inner']}>
-      <h1 className="fontStyle">Hello---React-Cli</h1>
-      <View store={store}></View>
-    </div>;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
   }
 }
 
